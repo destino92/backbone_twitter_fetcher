65bd5f797b703f88a520cf8d8fe6b68e14491ee2
destino92 <destinodello@gmail.com>
2016-06-08 12:53:06 +0300

    feat: instantiate searchView in app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 var timeline = new com.destino.view.TimelineView();
 var profileView = new com.destino.view.ProfileView();
 var searchModel = new com.destino.model.Search();
+var searchView = new com.destino.view.SerachView(model: searchModel);
