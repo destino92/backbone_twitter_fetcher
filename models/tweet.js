d2bf92984509b615b88d0077b8c0641339a4055d
destino92 <destinodello@gmail.com>
2016-06-08 09:51:29 +0300

    feat: added a timestamp to the tweet model and displays it in the timeline template.

--- models/tweet.js
+++ models/tweet.js
@@ -2,5 +2,11 @@ var com = com || {};
 com.destino = com.destino || {};
 com.destino.model = com.destino.model || {};
 com.destino.model.Tweet = Backbone.Model.extend({
-
+	parse: function(model){
+		// Use moment.js to format the date at witch the tweet was create.
+		var friendly = moment(model.created_at, "ddd MMM DD HH:mm:ss ZZ YYYY").fromNow();
+		// had timestamp has a model property
+		model.friendlyDate = friendly;
+		return model;
+	}
 });
