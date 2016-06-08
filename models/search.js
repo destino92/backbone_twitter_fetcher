80049df9f021bef116ea3b4520ae23d2bde2655e
destino92 <destinodello@gmail.com>
2016-06-08 10:48:00 +0300

    feat: added a search model.

---
+++ models/search.js
@@ -0,0 +1,14 @@
+var com = com || {};
+com.destino = com.destino || {};
+com.destino.model = com.destino.model || {};
+com.destino.model.Tweet = Backbone.Model.extend({
+	url: 'http://localhost:8080/search',
+	sync: function(method, model, options){
+		if(this.get('query')){
+			this.url = this.url + "/" + this.get("query");
+		}
+		Backbone.sync.call(this, method, model, options);
+		// reset url after the search
+		this.url = 'http://localhost:8080/search';
+	}
+});
