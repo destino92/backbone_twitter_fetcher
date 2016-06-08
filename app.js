7312b9930ae24b8e41446a56d1e86b5afdea738b
destino92 <destinodello@gmail.com>
2016-06-08 15:09:13 +0300

    fix: fixed typo in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var timeline = new com.destino.view.TimelineView();
 var profileView = new com.destino.view.ProfileView();
 var searchModel = new com.destino.model.Search();
-var searchView = new com.destino.view.SerachView(model: searchModel);
-var appRouter = new com.destino.router.AppRouter(searchModel: searchModel);
+var searchView = new com.destino.view.SerachView({model: searchModel});
+var appRouter = new com.destino.router.AppRouter({searchModel: searchModel});
 Backbone.history.start();
